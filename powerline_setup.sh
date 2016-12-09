f5326025753542790c1e216a25e9adf8171746fb
Yukihiro Saito <yukky@ubi.cs.ritsumei.ac.jp>
2016-12-09 12:03:45 -0800

    Fixed bug in powerline_setup.sh

--- powerline_setup.sh
+++ powerline_setup.sh
@@ -14,9 +14,9 @@ cd fonts
 cd ~
 
 echo "function _update_ps1() {
-    PS1=\"$(~/powerline-shell.py $? 2> /dev/null)\"
+    PS1=\"\$(~/powerline-shell.py \$? 2> /dev/null)\"
 }
 
-if [ \"$TERM\" != \"linux\" ]; then
-    PROMPT_COMMAND=\"_update_ps1; $PROMPT_COMMAND\"
+if [ \"\$TERM\" != \"linux\" ]; then
+    PROMPT_COMMAND=\"_update_ps1; \$PROMPT_COMMAND\"
 fi" >> ~/.bashrc
