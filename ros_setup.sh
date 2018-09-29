cb4e832d38b0862c2c96e390136a67b1222ee90c
Yukihiro Saito <yukky.saito@gmail.com>
2018-09-29 21:19:47 +0900

    update for ros kinetic

--- ros_setup.sh
+++ ros_setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# for Ubuntu14.04
+# for Ubuntu16.04
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
 sudo apt-get update
 
-sudo apt-get -y install ros-indigo-desktop-full
+sudo apt-get -y install ros-kinetic-desktop-full
 
 sudo rosdep init
 
 rosdep update
 
-echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
